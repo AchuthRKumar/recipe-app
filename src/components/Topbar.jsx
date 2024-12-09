e544adffb5e8c5f404e203e91d961196faaa6a83
AchuthRKumar <achusince2002@gmail.com>
2024-12-09 23:03:18 +0530

    working on nav bar

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,26 @@
+import { Text, Input, HStack, Center, Link } from "@chakra-ui/react"
+import { InputGroup } from "../components/ui/input-group"
+import { FaBowlFood } from "react-icons/fa6";
+
+function Topbar() {
+    return(
+        <div>
+            <Center>
+                <HStack gap="7">
+                    <Link href="#">Home</Link>
+                    <Link href="#">About</Link>
+                    <Text textStyle="5xl" margin="5px" className="tangerine-font"> Get Cooking </Text>
+                    <Link href="#">Recipes</Link>
+                    <Link href="#">Shop</Link>
+                </HStack>
+            </Center>
+            <Center>
+                <InputGroup startElement={<FaBowlFood/>}>
+                    <Input placeholder="Search for a Dish or Ingredient" width="300px" variant="flushed" />
+                </InputGroup>
+            </Center>
+        </div>
+    );
+}
+
+export default Topbar;
